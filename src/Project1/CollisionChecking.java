01608ca868e0304804bbfea6aac793bc3f0e9a0f
Leonos <covic416@gmail.com>
2023-10-02 13:59:54 -0400

    Create CollisionChecking.java
    Might use GJK algorithm

---
+++ src/Project1/CollisionChecking.java
@@ -0,0 +1,17 @@
+package Project1;
+
+import Project1.CreateScene.Obstacle;
+
+//Use either SAT with voronoi region or BSP for beginning, and then other algorithm for repaint (AABB)? GJK!
+public class CollisionChecking 
+{
+	public void checkCollision(Obstacle[] obstacles)
+	{
+		
+	}
+	
+	public void findFurthestPoint(int direction)
+	{
+		
+	}
+}
